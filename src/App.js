a3d29b2c206a606428944f283849add71321cd26
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-10-30 21:57:42 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,122 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+
+
+
+function ProjectList({projects}) {
+
+  const items = projects.map( project => 
+    <img className="projects" src = {project.img}/> 
+  );
+
+  return (
+    <div>
+      {items}
+    </div>
+  );
+
+}
+
+function Toolbar({filters, selected, onSelectFilter}) {
+
+  const items = filters.map( filter => 
+      <div key={filter} className="filter" onClick={() => onSelectFilter(filter)}>
+        {filter}
+      </div> 
+  );
+
+  return ( 
+  <div className="filters">
+    {items}
+  </div>
+  );
+
+}
+
+function Portfolio() {
+  const [selectedFilter, setSelectedFilter] = useState("All");
+  const filters = [ "All", "Websites", "Flayers" , "Business Cards"];
+
+  const projects = [{
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/mon.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/codystretch.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_003.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290.png",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/200.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_1.png",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_2.png",
+    category: "Flayers"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/the_ninetys_brand.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/dia.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/emi_haze.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/transmission.jpg",
+    category: "Business Cards"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/Triangle_350x197_1.jpg",
+    category: "Websites"
+  }, {
+    img: "https://raw.githubusercontent.com/netology-code/ra16-homeworks/master/events-state/filter/img/place200x290_3.png",
+    category: "Flayers"
+  }];
+  
+  const filteredProjects = selectedFilter === "All" 
+      ? projects
+      : projects.filter(project => project.category === selectedFilter);
+  
+
+  const onSelectFilter = (filter) => {
+    setSelectedFilter(filter);
+  };
+
+
+  return (
+    <>
+      <Toolbar
+        filters={filters}
+        selected={selectedFilter}
+        onSelectFilter={onSelectFilter}
+      />
+      <ProjectList projects = {filteredProjects}/>
+    </>
+  );
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Portfolio/>
   );
 }
 
